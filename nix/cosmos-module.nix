a55ec468344c4abea09b4bf8f979aa3ee6b763e6
Sam Hart <sam@hxrts.com>
2025-04-04 23:16:20 -0400

    fix: Avoid sourcing cargo env file in script

--- nix/cosmos-module.nix
+++ nix/cosmos-module.nix
@@ -48,7 +48,8 @@
             if ! command -v rustc > /dev/null; then
               echo "Installing Rust..."
               curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-              source "$HOME/.cargo/env"
+              # Instead of sourcing, we'll add to PATH directly
+              export PATH="$HOME/.cargo/bin:$PATH"
             fi
             
             # Build libwasmvm
